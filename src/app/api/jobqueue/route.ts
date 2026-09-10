6e1bd90ff9e457e41463bcc29ed526dd3c9396d9
arzyl <arzyllorenzo1212@gmail.com>
2026-09-10 15:28:29 +0800

    Refactor GET request to improve code readability

--- src/app/api/jobqueue/route.ts
+++ src/app/api/jobqueue/route.ts
@@ -9,8 +9,16 @@ const db = drizzle(process.env.DATABASE_URL!);
 
 export async function GET(request: Request) {
     
-    const job = (await db.select().from(jobTable).where(or(eq(jobTable.status, 'Pending'),eq(jobTable.status, 'In Progress'))).orderBy(desc(jobTable.dateCreated));
-
+    const job = await db
+  .select()
+  .from(jobTable)
+  .where(
+    or(
+      eq(jobTable.status, 'Pending'),
+      eq(jobTable.status, 'In Progress')
+    )
+  ) // <-- Close the .where() method here
+  .orderBy(desc(jobTable.dateCreated));
     return new Response(JSON.stringify(job),{
         status: 200,
         headers: { 'Content-Type': 'application/json' }
